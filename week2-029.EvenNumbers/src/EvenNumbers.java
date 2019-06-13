d61caba54502ab7eed5dd6ef176d046a1c634d0b
goan3392 <47539861+goan3392@users.noreply.github.com>
2019-06-13 16:44:59 -0400

    Add files via upload

---
+++ week2-029.EvenNumbers/src/EvenNumbers.java
@@ -0,0 +1,14 @@
+
+public class EvenNumbers {
+
+    public static void main(String[] args) {
+        // Write your code here
+        int even = 2;
+        while(true){
+            
+            System.out.println(even);
+            if(even ==100 )break;
+            even+=2;
+        }
+    }
+}
