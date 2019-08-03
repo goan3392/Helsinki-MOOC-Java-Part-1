a09ae97c8c91ecf689d6843173c21dc0d931aea3
goan3392 <47539861+goan3392@users.noreply.github.com>
2019-08-03 08:26:40 -0400

    Add files via upload

---
+++ week4-074.Multiplier/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        // This is an empty main method which you can use to test your class's functionality.
+        Multiplier num1 = new Multiplier(3);
+        System.out.println(num1.multiply(2));
+    }
+}
---
+++ week4-074.Multiplier/src/Multiplier.java
@@ -0,0 +1,12 @@
+
+public class Multiplier {
+    private int num;
+    
+    public Multiplier(int number){
+        this.num = number;
+    }
+    
+    public int multiply(int othernumber){
+        return this.num * othernumber;
+    }
+}
