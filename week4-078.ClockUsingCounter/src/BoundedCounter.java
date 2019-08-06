211359ad60492c53069e145a866a447ac557a4fb
goan3392 <47539861+goan3392@users.noreply.github.com>
2019-08-05 21:00:39 -0400

    Add files via upload

---
+++ week4-078.ClockUsingCounter/src/BoundedCounter.java
@@ -0,0 +1,33 @@
+
+public class BoundedCounter {
+    private int value;
+    private int upperLimit;
+    
+    public BoundedCounter(int upperLimit){
+        this.value = 0;
+        this.upperLimit = upperLimit;
+
+    }
+    
+    public void next() {
+        if(this.value == this.upperLimit){
+            this.value = -1;
+        }
+        this.value++;
+    }
+
+    public String toString() {
+        if(this.value<10)return "" + 0 + this.value;
+        return "" + this.value;
+    }
+    
+    public int getValue(){
+        return this.value;
+    }
+    
+    public void setValue(int newValue){
+        if(newValue>0 && newValue<=this.upperLimit){
+            this.value = newValue;
+        }
+    }
+}
